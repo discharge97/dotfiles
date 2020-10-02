afda76f1d15728f0b93e318b142006b5b5a06e81
Alex <i00jko@outlook.com>
2020-10-02 23:07:33 +0200

    changed apply sh

--- apply_dotFiles.sh
+++ apply_dotFiles.sh
@@ -1,11 +1,11 @@
 echo "Copying .bashrc"
-cp .bashrc ~/
+cp -u .bashrc ~/
 
 echo "Copying .bash_aliases"
-cp .bash_aliases ~/
+cp -u .bash_aliases ~/
 
 echo "Copying .config file"
-cp -r .config ~/
+cp -u -r .config ~/
 
 echo "Soursing bashrc"
 source ~/.bashrc
