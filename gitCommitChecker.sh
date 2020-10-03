520a1259bc5302c4398f8477d4c29c307e079512
Aleksandar Stojadinovic <aleksandar.stojadinovic.3641@metropolitan.ac.rs>
2020-10-03 13:53:46 +0200

    updated gitCommitChecker script and added ~/bin to /home/alex in bashrc

--- gitCommitChecker.sh
+++ gitCommitChecker.sh
@@ -1,18 +1,17 @@
 #!/bin/sh
 
-#60 seconds times <5> minutes
-sleep_time=60 * 5
-while :
-do
-	git fetch
-	gitBranch=$(git branch --show-current)
-	msg=$(git log HEAD..origin/$gitBranch --oneline)
+if $(git rev-parse --is-inside-work-tree);
+then
+	while :
+	do
+		git fetch
+		gitBranch=$(git branch --show-current)
+		msg=$(git log HEAD..origin/$gitBranch --oneline)
 
-	if [[ ! $gitBranch =~ "fatal" ]] && [ ! -z "$msg" ]
-	then
-		#printf "$msg\n"
-
-		notify-send -i ~/Pictures/git.png "Git status tracker" "Your branch ($gitBranch) is behind following commits!\n$msg"
-	fi
-	sleep $sleep_time
-done
+		if [[ ! $gitBranch =~ "fatal" ]] && [ ! -z "$msg" ]
+		then
+			notify-send -i ~/Pictures/git.png "Git status tracker" "Your branch ($gitBranch) is behind following commits!\n$msg"
+		fi
+	sleep 5m
+	done
+fi
