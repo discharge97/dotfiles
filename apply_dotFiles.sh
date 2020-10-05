e0fe5b1e5dd65ec968bf4eab831ad4d25af09710
Aleksandar <aleksandar.stojadinovic@metropolitan.ac.rs>
2020-10-05 09:47:28 +0000

    fixed gitCommitTracker script not having execute rights after ccp

--- apply_dotFiles.sh
+++ apply_dotFiles.sh
@@ -1,6 +1,6 @@
-echo -e "                                     
- ____  _         _                   
-|    \|_|___ ___| |_ ___ ___ ___ ___ 
+echo -e "
+ ____  _         _
+|    \|_|___ ___| |_ ___ ___ ___ ___
 |  |  | |_ -|  _|   | .'|  _| . | -_|
 |____/|_|___|___|_|_|__,|_| |_  |___|
                             |___|    "
@@ -9,7 +9,7 @@ printf "\n\n============ Updating dot files ============\n\n"
 cp -v -u .bashrc ~/
 cp -v -u .bash_aliases ~/
 cp -v -u -r .config ~/
-cp -v -u -r bin ~/
+cp -v -u -r bin ~/ && chmod +x ~/bin/apply_dotFiles.sh
 
 printf "\n\n#Sourcing bashrc: "
 source ~/.bashrc
