9adaf2feec1b04026f7a1e03fd6dccb7cdcd675a
Alexander <i00jko@outlook.com>
2020-10-31 17:13:59 +0100

    added script for workspace managment

---
+++ bin/i3wss.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+prompt=""
+
+case "$1" in
+	"move")        prompt="send to";;
+	"move-switch") prompt="move and switch to";;
+	*)             prompt="go to";;
+esac
+
+
+WORKSPACES=$(i3-msg -t get_workspaces | jq | grep name | sed 's/.*:\ \"\(.*\)\".*/\1/g' | sort)
+
+WS="$(echo $WORKSPACES | tr ' ' '\n' | dmenu -fn 'Hack-10' -p "$prompt: ")"
+
+case "$1" in
+	"move")        i3-msg "move container to workspace $WS" ;;
+	"move-switch") i3-msg "move container to workspace $WS; workspace $WS" ;;
+	*)             i3-msg "workspace $WS" ;;
+esac
+
