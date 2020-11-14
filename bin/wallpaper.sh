63e374700a8d2edb67c99f34124d987b14f34a7f
Alex <i00jko@outlook.com>
2020-11-14 16:14:48 +0100

    added gui image slector for wallpaper setting & i3 conf for picom

--- bin/wallpaper.sh
+++ bin/wallpaper.sh
@@ -6,16 +6,25 @@ help="
 \t-s = set wallpaper\n
 "
 
-while getopts hRs: flag
+while getopts ghRs: flag
 	do
-	    case "${flag}" in
-	        s)
+		case "${flag}" in
+		s)
 			feh --bg-fill ${OPTARG}
 			echo ${OPTARG} > ~/bin/wall.path;;
 		R)
-			wall=`cat ~/bin/wall.path`
+			wall="$(cat ~/bin/wall.path)"
 			feh --bg-fill "${wall}";;
 
-	        *) echo -e $help;;
+		g) path="$(sxiv -to ~/Pictures/wallpaper/)"
+			if [ -f "$path" ]; then
+				echo "$path" > ~/bin/wall.path
+				feh --bg-fill "$path"
+			else
+				echo -e "Image path not selected! \nUse 'm' key to select an image."
+			fi
+			;;
+
+	    *) echo -e $help;;
 	    esac
 	done
