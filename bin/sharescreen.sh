f2a5ca43be13053a6236faefcd3494f67fbf151e
Alex <i00jko@outlook.com>
2020-11-15 12:12:47 +0100

    added refreshrate flag

--- bin/sharescreen.sh
+++ bin/sharescreen.sh
@@ -6,13 +6,15 @@ help="
 \t-r = set output resolution | default: 1920x1080)\n
 \t-x = offset X | default: 0\n
 \t-y = offset Y | default: 0\n
+\t-R = set refresh rate | default: 30\n
 \t-h = help\n
 "
 x=0
 y=0
 res='1920x1080'
+rate=30
 
-while getopts x:y:r:h flag
+while getopts x:y:R:r:h flag
 	do
 		case "${flag}" in
 		x)
@@ -21,9 +23,11 @@ while getopts x:y:r:h flag
 			y="${OPTARG}";;
 		r)
 			res="${OPTARG}";;
+		R)
+			rate="${OPTARG}";;
 
 	    *) echo -e $help;;
 	    esac
 	done
 
-ffmpeg -f x11grab -r 30 -s "$res" -i :0.0+"$x","$y" -vcodec rawvideo -vf hflip -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0
+ffmpeg -f x11grab -r $rate -s "$res" -i :0.0+"$x","$y" -vcodec rawvideo -vf hflip -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0
